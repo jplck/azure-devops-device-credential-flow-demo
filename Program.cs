f8e265a3a56430b3767b48e303125a0570f7d98b
jplck <pollack.jan@gmail.com>
2021-02-24 11:05:52 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace azure_devops_oauth_device_login
+{
+    class Program
+    {
+        internal const string baseUrl = "https://dev.azure.com/";
+        internal const string ADOResourceId = "499b84ac-1321-427f-aa17-267ca6975798";
+        private static string _FullUrl = string.Empty;
+
+        public static void Main(string tenantId, string clientId, string orgName)
+        {
+            _ = tenantId ?? throw new ArgumentNullException("TenantId cannot be null");
+            _ = clientId ?? throw new ArgumentNullException("ClientId cannot be null");
+            _ = orgName ?? throw new ArgumentNullException("OrgId cannot be null");
+
+            _FullUrl = $"{baseUrl}{orgName}";
+
+            AuthenticationContext ctx = GetAuthenticationContext(tenantId);
+            AuthenticationResult result = null;
+            try
+            {
+                DeviceCodeResult codeResult = ctx.AcquireDeviceCodeAsync(ADOResourceId, clientId).Result;
+                Console.WriteLine("You need to sign in.");
+                Console.WriteLine("Message: " + codeResult.Message + "\n");
+                result = ctx.AcquireTokenByDeviceCodeAsync(codeResult).Result;
+
+                var bearerAuthHeader = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+                ListProjects(bearerAuthHeader);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Something went wrong.");
+                Console.WriteLine("Message: " + ex.Message + "\n");
+            }
+        }
+
+        private static AuthenticationContext GetAuthenticationContext(string tenant)
+        {
+            AuthenticationContext ctx = null;
+            if (tenant != null)
+                ctx = new AuthenticationContext("https://login.microsoftonline.com/" + tenant);
+            else
+            {
+                ctx = new AuthenticationContext("https://login.windows.net/common");
+                if (ctx.TokenCache.Count > 0)
+                {
+                    string homeTenant = ctx.TokenCache.ReadItems().First().TenantId;
+                    ctx = new AuthenticationContext("https://login.microsoftonline.com/" + homeTenant);
+                }
+            }
+
+            return ctx;
+        }
+
+        private static void ListProjects(AuthenticationHeaderValue authHeader)
+        {
+            // use the httpclient
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("User-Agent", "VstsRestApiSamples");
+                client.DefaultRequestHeaders.Add("X-TFS-FedAuthRedirect", "Suppress");
+                client.DefaultRequestHeaders.Authorization = authHeader;
+
+                // connect to the REST endpoint            
+                HttpResponseMessage response = client.GetAsync($"{_FullUrl}/_apis/projects?api-version=6.1-preview.4").Result;
+
+                // check to see if we have a succesfull respond
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("\tSuccesful REST call");
+                    Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                {
+                    throw new UnauthorizedAccessException();
+                }
+                else
+                {
+                    Console.WriteLine("{0}:{1}", response.StatusCode, response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
